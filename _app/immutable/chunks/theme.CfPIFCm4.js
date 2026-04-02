e4e1f38b07e4389f22ff4c1baa77c34b8594e105
yojoe <yousefsuleiman10@gmail.com>
2026-04-02 12:18:29 -0500

    Updates

--- _app/immutable/chunks/theme.CfPIFCm4.js
+++
@@ -1,1 +0,0 @@
-import{w as l}from"./paths.D2VOeKnl.js";import{g as c}from"./scheduler.gNMUaPUc.js";function i(){let e;try{e=!!JSON.parse(localStorage.getItem("theme")??"false")}catch{e=!1}const{subscribe:r,set:a}=l(e);function s(){const o=!c({subscribe:r});localStorage.setItem("theme",JSON.stringify(o)),a(o)}return{subscribe:r,toggle:s}}let t;function g(){return t||(t=i()),t}export{g};
--- _app/immutable/entry/start.B2Y7W2Vs.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CVaMxe2v.js";export{t as start};
